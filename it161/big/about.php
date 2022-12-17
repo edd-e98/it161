a2dd6ccadb7bb82a5032615a2f5104a1a02df377
Eddie <87498041+edd-e98@users.noreply.github.com>
2022-12-16 19:59:39 -0800

    Update about.php

--- it161/big/about.php
+++ it161/big/about.php
@@ -2,10 +2,11 @@
     include('./includes/header.php');
 ?>
     <main>
-        <div class="section" id= "aboutUs">
-            <h1 style="text-align: center">About Us</h1>
-            <p>    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="section justify" id= "aboutUs">
+            <h1>About Us</h1>
+            <p>House of Cox is a Seattle-based drag art collective that celebrates all things creepy, weird, and gender-diverse.</p>
+            <p>First built in 1632, the House of Cox itself has a long-standing history of both queer and paranormal activity. Experts estimate that as many as 7.2 ghosts haunt the house, but the most infamous is the ghost of Jack Cox. Jack moved to House of Cox in the early 1920s. This is also when the house became known as the House of Cox, since small children would throw rocks at the building and make fun of it's inhabitant for sharing a last name with a euphanism for human genitalia. Legend says that one such rock flew in through a window at the front of the house and hit Jack in the eye, piercing through to his brain and killing him instantly. To this day, his ghost haunts the establishment.</p>
+            <p>Today, House of Cox hosts drag shows, produced and performed by the students that live there and their friends. Their shows specialize in alternative, DIY drag that views gender through an inherently performative lense. In other words-- if gender is a performance, we're having as much with it as we can. Just rubbing our greasy little hands all over it.</p>
         </div>
     </main>
 <?php
