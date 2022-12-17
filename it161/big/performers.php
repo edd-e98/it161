229dfdc45b9408a69e01bfdb2459fdb56b5a834b
Eddie <87498041+edd-e98@users.noreply.github.com>
2022-12-16 20:07:54 -0800

    Update performers.php

--- it161/big/performers.php
+++ it161/big/performers.php
@@ -6,12 +6,45 @@
             <h1 style="text-align: center">Meet the Performers</h1>
             <div class="my-row">
                 <div class="my-column-short">
-                    <img class="performer-thumbnail fade" src="./images/performers/moana.png" alt="princess moana">
+                    <img class="performer-thumbnail fade" src="./images/performers/nik.png" alt="nik romantik">
                 </div>
                 <div class="my-column-long">
-                    <h2 class="bio-left">Moana</h2>
-                    <p class="bio-left">she/her</p>
-                    <p class="bio-left">that's right, guys. Moana does drag now. Why? because I said so, thats why.</p>
+                    <h2 class="bio-left">Nik Romantik</h2>
+                    <p class="bio-left">he/him</p>
+                    <p class="bio-left">Nik Romantik is Seattle's favorite up-and-coming drag thing, and co-founder of the House of Cox. We're not really sure who keeps letting this guy in here, but his goth and horror inspired performances are usually pretty energetic... Despite the fact that he's usually dead.</p>
+                    <p class="bio-left"><a class="bio-left" href="">instagram</a></p>
+                </div>
+            </div>
+
+            <div class="my-row">
+                <div class="my-column-long">
+                    <h2 class="bio-right">Maxine Milkers</h2>
+                    <p class="bio-right">she/her</p>
+                    <p class="bio-right">Maxine is the Big Titty Little Sissy of the Pacific Northwest. Her drag is inspired by sex and horror. Of course, you’ve gotta add a little humor in there as well! She’s been working with the House of Cox since February 2022.</p>
+                    <p class="bio-right"><a class="bio-right" href="">instagram</a></p>
+                </div>
+                <div class="my-column-short">
+                <img class="performer-thumbnail fade" src="./images/performers/maxine.png" alt="maxine milkers">
+                </div>
+            </div>
+        </div>
+    </main>
+<?php
+    include('./includes/footer.php');
+?><?php 
+    include('./includes/header.php');
+?>
+    <main>
+        <div class="section">
+            <h1 style="text-align: center">Meet the Performers</h1>
+            <div class="my-row">
+                <div class="my-column-short">
+                    <img class="performer-thumbnail fade" src="./images/performers/nik.png" alt="nik romantik">
+                </div>
+                <div class="my-column-long">
+                    <h2 class="bio-left">Nik Romantik</h2>
+                    <p class="bio-left">he/him</p>
+                    <p class="bio-left">Nik Romantik is Seattle's favorite up-and-coming drag thing, and co-founder of the House of Cox. We're not really sure who keeps letting this guy in here, but his goth and horror inspired performances are usually pretty energetic... Despite the fact that he's usually dead.</p>
                     <p class="bio-left"><a class="bio-left" href="">instagram</a></p>
                 </div>
             </div>
