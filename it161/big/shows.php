78610a7752a707b9e9f1aef4ee53c56655075f32
Eddie <87498041+edd-e98@users.noreply.github.com>
2022-12-16 20:09:05 -0800

    Update shows.php

--- it161/big/shows.php
+++ it161/big/shows.php
@@ -4,15 +4,15 @@
     <main>
         <div class="section">
             <h1>UPCOMING SHOWS</h1>
-            <h2 style="font-weight: bold; margin-bottom: 10px;">Playing it Straight</h2>
-            <h2 style="margin-bottom: 10px;">Friday, February 10th, 2023</h2>
+            <h2><b>Playing it Straight</b></h2>
+            <h2>Friday, February 10th, 2023</h2>
             <p>House of Cox Presents: <i>Playing it Straight</i>, a drag parody. Are you tired of attending local drag shows only to be accosted on all sides by queers, queens and fags? House of Cox is aiming to fix that. This year's valentines show will be upholding traditional gender roles and wholesome family values, so grab your sweetie and come see some sanitized drag!</p>
             <p>Doors at 7, show at 8. Tickets $10 each. Mask and Vax required for entry. This show will be RSVP only; please, check back soon for an RSVP form.</p>
         </div>
         <div class="section">
             <h1 >PAST SHOWS</h1>
-            <h2 style="margin-bottom: 10px;">Crypt of Cox</h2>
-            <h2 style="margin-bottom: 10px;">Friday, October 21st, 2022</h2>
+            <h2>Crypt of Cox</h2>
+            <h2>Friday, October 21st, 2022</h2>
             <!-- !!!!!IMAGE CAROUSEL!!!!! -->
             <div class="slideshow-container fade">
                 <div class="slideshow-image-div fade">
@@ -36,7 +36,7 @@
                 <li>Indica Domina <i>as the Watermelon</i></li>
                 <li>Roman Chargez <i>as Sasuke</i></li>
                 <li>Cloud Nine Inches  <i>as the Angel and the Devil</i></li>
-                <li>Maxine Milkers <i>returned as a Ghost</i></li>
+                <li>Maxine Milkers <i>returned as the Ghost</i></li>
                 <li>Ivan de Suc <i>as the Vampire<i/></li>
             </ul>
         </div>
